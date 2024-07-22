4b70833d57644289050b2ed9b59b5b7442628bd0
bclaveau <bclaveau@generixgroup.com>
2024-07-22 09:58:19 -0400

    test files, provider

---
+++ support/scripts/adapters/databases/flexible-server-mysql.sh
@@ -0,0 +1,15 @@
+__flexible-server-mysql_create() {
+  echo "a"
+}
+
+__flexible-server-mysql_create_super_user() {
+    echo "a"
+}
+
+__flexible-server-mysql_vault_configure() {
+  echo "a"
+}
+
+__flexible-server-mysql_get_default_port() {
+    echo "3306"
+}
